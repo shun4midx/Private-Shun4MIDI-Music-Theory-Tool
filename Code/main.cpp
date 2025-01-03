6b815d2dac16a26831c85b79ab2d9cbcc2aae717
Shun <shun4midx@Uranus-MacBook-Air.local>
2025-01-03 12:13:57 +0800

    Transfer to Replit

---
+++ Code/main.cpp
@@ -0,0 +1,9 @@
+/********************************************
+ * Copyright (c) 2025 Shun/順海 (@shun4midx) *
+*********************************************/
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;    
+}
