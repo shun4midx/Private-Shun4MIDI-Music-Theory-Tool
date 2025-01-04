6ac382c6fa7dbcd96d6ed9f1a2dc5a6ceb0fa5a2
shun4midx <shun4midx@gmail.com>
2025-01-05 05:16:24 +0800

    Rough structure for classes constructed

---
+++ Code/shunmusic/note_manip.cpp
@@ -0,0 +1,6 @@
+/********************************************
+ * Copyright (c) 2025 Shun/順海 (@shun4midx) *
+ * Project: Shun4MIDI Music Theory Tool     *
+ * File Type: Planning spec file            *
+ * File: note_manip.cpp                     *
+ ********************************************/
