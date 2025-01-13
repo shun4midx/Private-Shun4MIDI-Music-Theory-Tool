90f7ffe9ecea263d011f42fafb7225fde0790f9c
shun4midx <shun4midx@gmail.com>
2025-01-13 21:39:41 +0800

    My Japanese name change

--- Code/shunmusic/note_manip.h
+++ Code/shunmusic/note_manip.h
@@ -1,5 +1,5 @@
 /********************************************
- * Copyright (c) 2025 Shun/順海 (@shun4midx) *
+ * Copyright (c) 2025 Shun/翔海 (@shun4midx) *
  * Project: Shun4MIDI Music Theory Tool     *
  * File Type: Class header file             *
  * File: note_manip.h                       *
